7a941e7fe8fd7906ef41edfa085ed04b896955bb
Virlence5 <dongmovirlence@gmail.com>
2025-02-11 11:57:51 +0100

    Création et intégration du composant About.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 
+import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
+
       <div className="p-5 md:px-[15%]">
+
         <Navbar/>
+
         <Home />
+        
       </div>
+
+      <About/>
+
     </h1>
   )
 }
