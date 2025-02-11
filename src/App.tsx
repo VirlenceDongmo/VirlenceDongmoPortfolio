0d1d48ee8412fa7f9404db95c9192a2366a8608c
Virlence5 <dongmovirlence@gmail.com>
2025-02-11 09:15:20 +0100

    Création et intégration du composant Navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      <div>
+      <div className="p-5 md:px-[15%]">
         <Navbar/>
       </div>
     </h1>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,42 @@ import { Container } from "lucide-react"
 
 
 const Navbar = () => {
-  return (
-    <a href="#">
-        <Container />
-    </a>
-  )
+    return (
+        <div className="flex justify-center md:justify-between item-center p-4">
+            <a href="#" className="flex items-center font bold text-3xl md:text-xl">
+                <Container className="mr-2" />
+                Virlence
+                <span className="text-accent">Dev</span>
+            </a>
+
+            <ul className="hidden md:flex space-x-4">
+                <li>
+                    <a href="#" className="btn btn-sm btn-ghost">
+                        Accueil
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" className="btn btn-sm btn-ghost">
+                        A propos
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" className="btn btn-sm btn-ghost">
+                        Mes expériences
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" className="btn btn-sm btn-ghost">
+                        Mes projets
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+    )
 }
 
 export default Navbar
